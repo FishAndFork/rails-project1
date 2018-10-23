995d2bedffab5f7572aa7bd7b8ea0802757d9796
Fish and Fork <i9099dragon@gmail.com>
2018-10-23 23:16:34 +0300

    Add create functionality

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,18 @@
 class ArticlesController < ApplicationController
+    include ArticlesHelper
+
     def show
         @article = Article.find(params[:id])
     end
+
+    def new
+        @article = Article.new
+    end
+
+    def create
+        @article = Article.new(article_params)
+        @article.save
+        redirect_to article_path(@article)
+    end
+
 end
